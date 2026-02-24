497a463b5f4aaea2067af9c8aa311a72a6b04d43
Ana Silva <anasilva@Mac.lan>
2026-02-24 14:44:20 +1000

    fix: add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: '192.168.7.89'
-  // }
+  base: "/my-lift-web/"
 })
